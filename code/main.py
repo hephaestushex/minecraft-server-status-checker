4278864938fc5dcee80c0756ff9a58ae6e8ca302
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2026-06-25 17:44:26 -0600

    Wifi Connection code

--- code/main.py
+++ code/main.py
@@ -8,4 +8,17 @@ wlan.active(True)
 
 wlan.connect(secrets.ssid, secrets.password)
 
+connection_timeout = 10
+while connection_timeout > 0:
+    if wlan.status() >= 3:
+        break
+    connection_timeout -= 1
+    print("Waiting for WiFi")
+    sleep(1)
 
+if wlan.status() != 3:
+    raise RuntimeError('Failed to establish wifi connection')
+else:
+    print("Connection successful!")
+    network_info = wlan.ifconfig()
+    print("IP ADDR:", network_info[0])
